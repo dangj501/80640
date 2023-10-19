a3f6194c3e3f67319e1bae5ddeae9eaac8ab91b1
dangj501 <dangja9@gmail.com>
2023-10-19 12:55:29 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,13 +1,29 @@
 package mx.uv;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hola mundo" );
+
+        get("/", (request, response) -> {
+            return "<h1>Hola mundo</h1>";
+        });
+
+        get("/ruta1", (request, response) -> {
+            return "<h1>Adios mundo</h1>";
+        });
+
+        get("/ruta2", (request, response) -> {
+            return "<h1>Mundooooooooo</h1>";
+        });
+
+        get("/ruta3", (request, response) -> {
+            return "<h1>OK Mundo</h1>";
+        });
+
+
     }
 }
