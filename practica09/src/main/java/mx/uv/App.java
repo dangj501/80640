ce37d315cd23a915a98c592683146d3ed1f620e2
dangj501 <dangja9@gmail.com>
2023-11-09 20:59:18 -0600

    practica08 main modificado

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -7,23 +7,33 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola mundo" );
+        //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+        options("/*",(request,response)->{
+        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+        if(accessControlRequestHeaders!=null){
+            response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+        }
+        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+        if(accessControlRequestMethod!=null){
+            response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+            }
+        return "OK";
 
+});
+
+before((request,response)->response.header("Access-Control-Allow-Origin","*"));
         get("/", (request, response) -> {
             return "<h1>Hola mundo</h1>";
         });
-
-        get("/ruta1", (request, response) -> {
-            return "<h1>Adios mundo</h1>";
+         get("/ruta1", (request, response) -> {
+            return "<h1>Hola mundo XD</h1>";
         });
-
-        get("/ruta2", (request, response) -> {
-            return "<h1>Mundooooooooo</h1>";
+         get("/ruta2", (request, response) -> {
+            return "{'alumno':'Juan','Matricula':'vad','Carrera':'TC'}";
         });
-
-        get("/ruta3", (request, response) -> {
-            return "<h1>OK Mundo</h1>";
+         get("/ruta3", (request, response) -> {
+            return "<h1>Byeee</h1>";
         });
-
-
     }
 }
+
